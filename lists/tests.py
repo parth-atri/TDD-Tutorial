9e94af810e5d54f71da55a58d51c6bad2d1b942d
Parth Shah <shahpv@ATRI003362.local>
2025-07-09 14:48:13 -0700

    feat(TDD_tutorial-Chapter3): Testing a Simple Home Page with Unit Tests
    Added a unit test with a view function. Updated the url config to map / to home_page view.
    
    [ICTRAINING-2181]

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
